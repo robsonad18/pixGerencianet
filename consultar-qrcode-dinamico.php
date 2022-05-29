1369649a1f7cc7ec3c023cb82dc3807b186ad06a
Robson Farias <robsonfariasAD@gmail.com>
2022-05-29 15:58:03 -0300

    checkpoint

--- consultar-qrcode-dinamico.php
+++ consultar-qrcode-dinamico.php
@@ -1,12 +1,10 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/main.php';
+require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/main.php";
 
 $response = $obApiPix->consultCob("");
 
-if (!isset($response['location'])) {
-	echo 'Problemas ao consultar PIX dinamico:' . $response;
-}
+if (!isset($response["location"])) echo "Problemas ao consultar PIX dinamico:" . $response;
 
 echo $response;
--- gera-qrcode-estatico.php
+++ gera-qrcode-estatico.php
@@ -1,26 +1,22 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . "/vendor/autoload.php";
 
 use App\Pix\Payload;
 use Mpdf\QrCode\QrCode;
 use Mpdf\QrCode\Output;
 
-// Instancia principal do payload pix
 $obPayload = (new Payload)
-    ->setPixKey('1899999999')
+    ->setPixKey("1899999999")
     ->setDescription("Pagamento do pedido 666")
-    ->setMerchantName('Robson Lucas')
-    ->setMerchantCity('SAO PAULO')
-    ->setAmount('0')
+    ->setMerchantName("Robson Lucas")
+    ->setMerchantCity("SAO PAULO")
+    ->setAmount("0")
     ->setTxId(sha1(true));
 
-// Codigo de pagamento pix
 $payloadQrCode = $obPayload->getPayload();
-// QR Code
 $obQrCode = new QrCode($payloadQrCode);
-// Imagem do Qrcode
 $image = (new Output\Png)->output($obQrCode, 400);
 
-header('Content-Type: image/png');
+header("Content-Type: image/png");
 echo $image;
